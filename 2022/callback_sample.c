a4f5aad177ba95fbc9883e9bd60ca35df5496666
atjjnnn <50092173+atjjnnn@users.noreply.github.com>
2022-07-09 21:58:33 +0900

    fix: fix some commentout style

--- 2022/callback_sample.c
+++ 2022/callback_sample.c
@@ -5,18 +5,19 @@
 #include <unistd.h>
 #include <pthread.h>
 
-/* 要求APIの引数で渡す関数（コールバック）の型定義 */
+//要求APIの引数で渡す関数（コールバック）の型定義
 typedef void (*notify_event_cb)(const char *result);
-/* 要求用API。引数のeventが要求処理後に呼ばれる */
+
+//要求用API。引数のeventが要求処理後に呼ばれる
 static void request_action(notify_event_cb event);
 
 /* 要求を受ける側の処理 */
-/* メイン処理 */
+//メイン処理
 static void * actor_main(void * arg)
 {
 	notify_event_cb event = (notify_event_cb) arg;
 	sleep(1);
-	/* 処理が終わった想定。関数をコール */
+	//処理が終わった想定。関数をコール
 	event("Result OK!");
 	
 	return NULL;
